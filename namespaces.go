127e2d748179f2577607fbfa753b7af1dba98062
Jorge Alberto Díaz Orozco <jorge.diaz@riskident.com>
2023-12-11 21:05:27 +0100

    Add completion for the namespaces flag

---
+++ namespaces.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+	log "github.com/sirupsen/logrus"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+func getNamespaces(ctx context.Context, clientset kubernetes.Interface) []string {
+	namespaces, err := clientset.CoreV1().Namespaces().List(ctx, metav1.ListOptions{})
+	if err != nil {
+		log.Error(err)
+		return []string{}
+	}
+
+	namespaceList := make([]string, len(namespaces.Items))
+	for i, namespace := range namespaces.Items {
+		namespaceList[i] = namespace.Name
+	}
+	return namespaceList
+}
---
+++ namespaces_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	testclient "k8s.io/client-go/kubernetes/fake"
+	"testing"
+)
+
+func TestGetNamespaces(t *testing.T) {
+	ctx := context.Background()
+	clientset := testclient.NewSimpleClientset()
+	namespaces := getNamespaces(ctx, clientset)
+	assert.Len(t, namespaces, 0)
+
+	_, err := clientset.CoreV1().Namespaces().Create(
+		ctx,
+		&v1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "test",
+			},
+		},
+		metav1.CreateOptions{},
+	)
+	assert.NoError(t, err)
+
+	newNamespaces := getNamespaces(ctx, clientset)
+	assert.Len(t, newNamespaces, 1)
+	assert.Equal(t, "test", newNamespaces[0])
+}
