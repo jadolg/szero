e5a49881c00631e6708b2da2cd3c6993a603b934
Jorge Alberto Díaz Orozco (Akiel) <diazorozcoj@gmail.com>
2025-03-24 23:00:49 +0100

    Add statefulsets to the long description

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ var (
 	rootCmd     = &cobra.Command{
 		Use:   "szero",
 		Short: "Temporarily scale down/up/restart all deployments and statefulsets in a namespace",
-		Long:  "Downscale all deployments in a namespace to 0 replicas and back to their previous state. Useful when you need to tear everything down and bring it back in a namespace.",
+		Long:  "Downscale all deployments and statefulsets in a namespace to 0 replicas and back to their previous state. Useful when you need to tear everything down and bring it back in a namespace.",
 	}
 )
 
