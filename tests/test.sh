9e5fff2fe1276e173cdaaf7f7379eaea82aaa152
Jorge Alberto Díaz Orozco <jorge.diaz@riskident.com>
2025-03-28 12:43:42 +0100

    Add basic functionality (up/down) tests

--- tests/test.sh
+++ tests/test.sh
@@ -4,3 +4,20 @@ set -euo pipefail
 source ./tests/testing.sh
 
 ./szero -h
+
+kubectl apply -f ./tests/deployment.yaml
+kubectl apply -f ./tests/statefulset.yaml
+kubectl wait --for=condition=available deployment/testdeployment001 --timeout=60s
+kubectl wait --for=condition=ready pod -l app=teststatefulset001 --timeout=60s
+
+assert "kubectl get pods --no-headers | wc -l" "5"
+assert "kubectl get deployment testdeployment001 -o jsonpath='{.spec.replicas}'" "3"
+assert "kubectl get sts teststatefulset001 -o jsonpath='{.spec.replicas}'" "2"
+./szero down
+assert "kubectl get deployment testdeployment001 -o jsonpath='{.spec.replicas}'" "0"
+assert "kubectl get sts teststatefulset001 -o jsonpath='{.spec.replicas}'" "0"
+assert_eventually "kubectl get pods --no-headers | wc -l" "0"
+./szero up
+assert "kubectl get deployment testdeployment001 -o jsonpath='{.spec.replicas}'" "3"
+assert "kubectl get sts teststatefulset001 -o jsonpath='{.spec.replicas}'" "2"
+assert_eventually "kubectl get pods --no-headers | wc -l" "5"
--- tests/testing.sh
+++ tests/testing.sh
@@ -1,30 +1,23 @@
 function assert() {
-    if [[ "$1" != "$2" ]]; then
-      echo "Test failed: expected '$2', but got '$1'"
+    output=$(eval "$1")
+    if [[ $output != "$2" ]]; then
+      echo "Test failed: expected '$2', but got '$output'"
       exit 1
     fi
 }
 
-function assert_fail() {
-    if "$@"; then
-        echo "Test failed: command succeeded, but it was expected to fail"
-        exit 1
-    else
-        echo "Test passed: command failed as expected"
-    fi
-}
-
-function eventually() {
+function assert_eventually() {
     max_attempts=20
     for ((i=1; i<=max_attempts; i++)); do
-      if "$@"; then
-        echo "Command succeeded"
+      output=$(eval "$1")
+      if [[ $output == "$2" ]]; then
+        echo "Attempt $i succeeded"
         return 0
       else
-        echo "Attempt $i failed"
+        echo "Attempt $i failed - expected '$2', but got '$output'"
       fi
-      sleep 5
+      sleep 2
     done
-    echo "Command failed after $max_attempts attempts"
+    echo "Assert failed after $max_attempts attempts"
     exit 1
 }
